979e094f27f15a0ac4a20cae3f19cded058b9b5d
P4ti3nn3 <115619908+P4ti3nn3@users.noreply.github.com>
2023-12-12 13:53:11 +0100

    Create f4k3s3erv.py

---
+++ Scr1pts/f4k3s3erv.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3 
+
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import json
+
+class S(BaseHTTPRequestHandler):
+    def _set_headers(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'application/json')
+        self.end_headers()
+
+    def do_GET(self):
+        self._set_headers()
+        response_data_json = '{"token":"toto","id":"nofalse","balance":1000}'
+        response_data = json.loads(response_data_json)
+        tab = [response_data]
+        self.wfile.write(json.dumps(tab).encode('utf-8'))
+
+    def do_HEAD(self):
+        self._set_headers()
+
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length'])
+        post_data = self.rfile.read(content_length)
+        self._set_headers()
+        print(post_data.decode('utf-8'))
+        # Respond with a JSON containing "credential_id" equal to true 
+        response_data = '{"token":"toto","id":"nofalse","balance":1000, "mesage": "POST request received", "data_received": post_data.decode("utf-8")}'
+        response_data = json.loads(response_data_json)
+        tab = [response_data]
+        self.wfile.write(json.dumps(tab).encode('utf-8'))
+
+def run(server_class=HTTPServer, handler_class=S, port=6673):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    print(f'Starting httpd on port {port}...')
+    httpd.serve_forever()
+
+if __name__ == "__main__":
+    from sys import argv
+    if len(argv) == 2:
+        run(port=int(argv[1]))
+    else:
+        run()
